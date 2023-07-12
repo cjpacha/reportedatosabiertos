455d4b4021629f32af74d9362b8b43cd1f266523
eliz121 <139375055+eliz121@users.noreply.github.com>
2023-07-12 16:19:01 -0500

    vse define una variable para recibir los datos

--- api.js
+++ api.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 
 axios.get('https://datosabiertos.gob.ec/api/3/action/package_list')
   .then(response => {
-    let listadataset = response.data;
-    console.log(listadataset.result);
+    let listadataset = response.data.result;
+    console.log(listadataset);
   })
   .catch(error => {
     console.error(error);
