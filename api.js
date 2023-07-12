6fc57f93842f74a21b88c49b74159967279ce977
eliz121 <139375055+eliz121@users.noreply.github.com>
2023-07-12 16:11:36 -0500

    se crea el proyecto e instala axios
    npm init -y
    npm i axios

--- api.js
+++ api.js
@@ -2,7 +2,8 @@ const axios = require('axios');
 
 axios.get('https://datosabiertos.gob.ec/api/3/action/package_list')
   .then(response => {
-    console.log(response.data);
+    let listadataset = response.data;
+    console.log(listadataset.result);
   })
   .catch(error => {
     console.error(error);
