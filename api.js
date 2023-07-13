178af9230be5a8df02f7628f2a5cb6d1fab34d2b
eliz121 <139375055+eliz121@users.noreply.github.com>
2023-07-13 14:53:36 -0500

    Nuevo
    Nuevo

--- api.js
+++ api.js
@@ -1,7 +1,5 @@
-//inicio de desarrollo 11 de julio de 2023.
-// Maria Mendoza - Javier Pacha
-
-const axios = require('axios');
+ //Uso de api de datos abiertos
+ const axios = require('axios');
 
 axios.get('https://datosabiertos.gob.ec/api/3/action/package_list')
   .then(response => {
---
+++ reporte.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+axios.get('https://datosabiertos.gob.ec/api/3/action/package_list')
+  .then(response => {
+    let lista = response.data.result;
+
+    for (let i = 0; i < lista.length; i++) {
+      let urlconjutodatos = 'https://datosabiertos.gob.ec/api/3/action/package_show?id=' + lista[i];
+      axios.get(urlconjutodatos)
+        .then(response => {
+          let datosConjunto = response.data.result;
+
+          console.log(i + 1, datosConjunto.title);
+          console.log(datosConjunto.name);
+          console.log(datosConjunto.organization.title);
+          console.log(datosConjunto.metadata_created);
+          console.log(datosConjunto.metadata_modified);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
