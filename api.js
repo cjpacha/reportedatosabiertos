119f94de10b631dd9dbe1c05b1323cc6f3e33d43
Javier Pacha <javierpacha@gmail.com>
2023-07-13 13:59:40 -0500

    for

--- api.js
+++ api.js
@@ -6,7 +6,11 @@ const axios = require('axios');
 axios.get('https://datosabiertos.gob.ec/api/3/action/package_list')
   .then(response => {
     let listadataset = response.data.result;
-        console.log(listadataset);
+        for (i = 0; i < listadataset.length; i++) {
+          console.log(i+1, listadataset[i]);
+                  };
+        console.log("TOTAL DE CONJUTOS DE DATOS:" + i);
+
 
   })
   .catch(error => {
