d4443f9bb732c8f860f9a830a4ff9d93439bcbd1
Javier Pacha <javierpacha@gmail.com>
2023-07-13 16:49:28 -0500

    se comento unas impresiones

--- reporte.js
+++ reporte.js
@@ -6,20 +6,20 @@ axios.get('https://datosabiertos.gob.ec/api/3/action/package_list')
 
     for (let i = 0; i < lista.length; i++) {
       let urlconjutodatos = 'https://datosabiertos.gob.ec/api/3/action/package_show?id=' + lista[i];
+      
       axios.get(urlconjutodatos)
         .then(response => {
           let datosConjunto = response.data.result;
-
           console.log(i + 1, datosConjunto.title);
-          console.log(datosConjunto.name);
-          console.log(datosConjunto.organization.title);
-          console.log(datosConjunto.metadata_created);
-          console.log(datosConjunto.metadata_modified);
+          //console.log(datosConjunto.name);
+          //console.log(datosConjunto.organization.title);
+          //console.log(datosConjunto.metadata_created);
+          //console.log(datosConjunto.metadata_modified);
         })
         .catch(error => {
           console.error("------------error en el for interno");
         });
-    }
+    } ;
   })
   .catch(error => {
     console.error("+++++++++++++++++error enel for externo");
