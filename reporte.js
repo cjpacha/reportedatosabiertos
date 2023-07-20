9296e63fb6eb17ebb994b3e7c2e5c7f2a725356f
eliz121 <139375055+eliz121@users.noreply.github.com>
2023-07-20 14:32:55 -0500

    Excel con todos los datos necesarios

--- reporte.js
+++ reporte.js
@@ -1,27 +1,126 @@
+const fs = require('fs');
 const axios = require('axios');
+const excel = require('exceljs');
+
+async function obtenerConjuntosDeDatos() {
+  try {
+    const response = await axios.get('https://datosabiertos.gob.ec/api/3/action/package_list');
+    const lista = response.data.result;
+    return lista;
+  } catch (error) {
+    console.error("Error al obtener la lista de conjuntos de datos:", error.message);
+    throw error;
+  }
+}
+
+async function obtenerDatosConjunto(urlConjuntoDatos) {
+  try {
+    const response = await axios.get(urlConjuntoDatos);
+    return response.data.result;
+  } catch (error) {
+    console.error("Error al obtener datos del conjunto:", error.message);
+    throw error;
+  }
+}
 
 async function imprimir() {
   try {
-    const response = await axios.get('https://datosabiertos.gob.ec/api/3/action/package_list');
-    let lista = response.data.result;
+    const lista = await obtenerConjuntosDeDatos();
+    let sumaTotalRecursos = 0;
+
+    const workbook = new excel.Workbook();
+    const worksheet = workbook.addWorksheet('Datos');
+
+    worksheet.columns = [
+      { header: 'Título', key: 'title' },
+      { header: 'Nombre', key: 'name' },
+      { header: 'Organización', key: 'organization' },
+      { header: 'Fecha creación', key: 'created' },
+      { header: 'Fecha modificación', key: 'modified' },
+      { header: 'Num Recursos', key: 'num_resources' },
+      { header: 'URL Recurso', key: 'url' },
+      { header: 'Tamaño Recurso', key: 'size' },
+    ];
 
     for (let i = 0; i < lista.length; i++) {
-      let urlconjutodatos = 'https://datosabiertos.gob.ec/api/3/action/package_show?id=' + lista[i];
-
+      const urlConjuntoDatos = 'https://datosabiertos.gob.ec/api/3/action/package_show?id=' + lista[i];
       try {
-        const response = await axios.get(urlconjutodatos);
-        let datosConjunto = response.data.result;
-        console.log(i + 1, datosConjunto.title);
-        console.log(datosConjunto.name);
-        console.log(datosConjunto.organization.title);
-        console.log(datosConjunto.metadata_created);
-        console.log(datosConjunto.metadata_modified);
+        const datosConjunto = await obtenerDatosConjunto(urlConjuntoDatos);
+        const row = {
+          title: datosConjunto.title,
+          name: datosConjunto.name,
+          organization: datosConjunto.organization.title,
+          created: datosConjunto.metadata_created,
+          modified: datosConjunto.metadata_modified,
+          num_resources: datosConjunto.num_resources,
+          url: '', // Initialize with an empty string
+          size: '', // Initialize with an empty string
+        };
+
+        if (datosConjunto.resources && datosConjunto.resources.length > 0) {
+          for (let j = 0; j < datosConjunto.resources.length; j++) {
+            const recurso = datosConjunto.resources[j];
+            // Check if the resource has a URL
+            if (recurso.url) {
+              row.url += recurso.url + '\n';
+            } else {
+              row.url += 'Sin URL\n';
+            }
+
+            // Verifica si el recurso tiene tamaño
+            if (recurso.size) {
+              row.size += recurso.size + '\n';
+            } else {
+              row.size += 'Sin tamaño\n';
+            }
+          }
+        } else {
+          // Si no hay recursos se muestra un mensaje
+          row.url = 'Sin recursos';
+          row.size = 'Sin recursos';
+        }
+
+        worksheet.addRow(row);
+        imprimirDatos(i + 1, datosConjunto);
+
+        sumaTotalRecursos += datosConjunto.num_resources;
       } catch (error) {
-        console.error("Error");
+        continue;
       }
     }
+
+    console.log('Suma total de recursos:', sumaTotalRecursos);
+
+    workbook.xlsx.writeFile('data.xlsx')
+      .then(() => {
+        console.log('Generado correctamente');
+      })
+      .catch((err) => {
+        console.error('Error al guardar en Excel:', err.message);
+      });
+
   } catch (error) {
-    console.error("Error");
+    console.error("Error en la impresión:", error.message);
+  }
+}
+
+function imprimirDatos(index, datos) {
+  console.log(index, datos.title);
+  console.log(datos.name);
+  console.log(datos.organization.title);
+  console.log(datos.metadata_created);
+  console.log(datos.metadata_modified);
+  console.log(datos.num_resources);
+
+  if (datos.resources && datos.resources.length > 0) {
+    for (let j = 0; j < datos.resources.length; j++) {
+      const recurso = datos.resources[j];
+      console.log(`Recurso ${j + 1}:`);
+      console.log('URL Recurso:', recurso.url);
+      console.log('Tamaño Recurso:', recurso.size);
+    }
+  } else {
+    console.log('Recursos no disponibles');
   }
 }
 
